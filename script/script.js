ad196d33dc7ea58adb683ed956fc4916b732d4bd
pnttrrv <ndhkptr@gmail.com>
2025-01-16 15:45:55 +0700

    adding gallery, rsvp, and gifts section

--- script/script.js
+++ script/script.js
@@ -8,3 +8,34 @@ offCanvas.addEventListener('show.bs.offcanvas', function () {
 offCanvas.addEventListener('hidden.bs.offcanvas', function () {
   sticky.style.overflow = 'hidden';
 });
+
+let open = localStorage.getItem('status');
+if (!open) {
+  localStorage.setItem('status', 'unopened');
+}
+
+if (open === 'unopened') {
+  dissableScroll();
+}
+if (open === 'opened') {
+  enableScroll();
+}
+
+function dissableScroll() {
+  const rootElement = document.querySelector(':root');
+  scrollTop = window.pageXOffset || document.documentElement.scrollTop;
+  scrollLeft = window.pageYOffset || document.documentElement.scrollLeft;
+
+  window.onscroll = function () {
+    window.scrollTo(scrollTop, scrollLeft);
+  };
+  rootElement.style.scrollBehavior = 'auto';
+  localStorage.setItem('status', 'unopened');
+}
+
+function enableScroll() {
+  const rootElement = document.querySelector(':root');
+  window.onscroll = function () {};
+  rootElement.style.scrollBehavior = 'smooth';
+  localStorage.setItem('status', 'opened');
+}
